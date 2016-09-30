83b4ae4420f3b5eb93f3c35023e29abf8340c179
shimshock <shimshock70@gmail.com>
2016-09-29 20:03:24 -0700

    Final version of week 3 assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## This is a test
-## functions do
+## This function will cache the inverse of a matrix
+## When trying to invert a matrix it will first check to see if thee is a cached verison
+## if there is it will use it
 
-## Write a short comment describing this function
+## Creates the matrix in cache
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) {##defines the function
+    inv<- NULL
+    set<- function(y){
+        x<<-y
+        inv<-NULL
+    }
+    get<-function()x
+    setinverse<-function(inverse) inv<<-inverse
+    getinverse<-function() inv
+    list(set=set,get=get,
+          setinverse=setinverse,
+          getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## This provides the inverse of the matrix 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      inv <- x$getinverse()
+      if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+      }
+      data <- x$get()
+      inv <- solve(data, ...)
+      x$setinverse(inv)
+      inv
 }
